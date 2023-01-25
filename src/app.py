8796fd08ce830ddb8223e0c3e73504c484bf06c0
Sahil Pattni <sahilpattni97@gmail.com>
2023-01-25 12:58:43 -0500

    Integrated elements into Streamlit

--- src/app.py
+++ src/app.py
@@ -16,47 +16,89 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
     menu_items={
-        "About": 'COOLK'
+        "About": 'Equity Analysis Tool'
     }
 )
 
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(message)s')
 
-# --- GLOBALS --- #
-# Containers
-
-container_one = st.container()
-container_two = st.container()
-
+input_container = st.container()  # Input (ticker)
+info_container = st.container()   # Company Overview, Metrics, Candlestick Chart
 
 
 def load_data(ticker):
+    """
+    Load data from the API and display it on the page.
+
+    Parameters
+    ----------
+    ticker : str
+        The ticker of the company to load data for.
+    
+    Returns
+    -------
+    None
+    """
     logging.info('Loading data...')
-    overview, response_code = fin.company_overview(ticker)
-    daily_adjusted, _ = fin.daily_adjusted(ticker)
-    if response_code == 200:
-        container_two.empty()
-        logging.info('Data loaded successfully.')
-        container_two.header(f"{overview['Name']} ({overview['Symbol']})")
-        container_two.caption(f"{overview['Address']}")
-        container_two.caption(f"{overview['Description']}")
+    # Company Name, Address, Description
+    overview, response_code_overview = fin.company_overview(ticker)
+    # Daily Adjusted Prices
+    daily_adjusted, response_code_daily = fin.daily_adjusted(ticker)
+    
+    if response_code_overview == 200 and response_code_daily == 200:
+        try:
+            info_container.empty()
+            logging.info('Data loaded successfully.')
+            # Company Info
+            info_container.header(f"{overview['Name']} ({overview['Symbol']})")
+            info_container.caption(f"{overview['Address']}")
+            info_container.caption(f"{overview['Description']}")
 
-        # --- Metrics --- #
-        col1, col2, col3 = container_two.columns(3)
-        col1.metric(label='Market Cap', value=millify(overview['MarketCapitalization'], precision=3))
-        col2.metric(label='P/E Ratio', value=overview['PERatio'])
-        col3.metric(label='Dividend Yield', value=overview['DividendYield'])
+            # --- Metrics --- #
+            col1, col2, col3, col4 = info_container.columns(4)
+            col1.metric(label='Price', value=eg.get_latest_price(daily_adjusted))
+            col2.metric(label='Market Cap', value=millify(overview['MarketCapitalization'], precision=3))
+            col3.metric(label='P/E Ratio', value=overview['PERatio'])
+            col4.metric(label='Dividend Yield', value=overview['DividendYield'])
+            
+            # CandleStick Chart
+            info_container.plotly_chart(
+                eg.candlestick(daily_adjusted),
+                use_container_width=True
+            )
+            
+            # --- Income Statement & Balance Sheet --- #
+            col1, col2 = info_container.columns(2)
+            # Income Statement
+            income_statement, response_code = fin.income_statement(ticker)
+            if response_code == 200:
+                col1.subheader('Income Statement')
+                col1.dataframe(income_statement)
+            
+            # Balance Sheet
+            balance_sheet, response_code = fin.balance_sheet(ticker)
+            if response_code == 200:
+                col2.subheader('Balance Sheet')
+                col2.dataframe(balance_sheet)
 
-        
+        except Exception as e:
+            # Print error message and stack trace
+            logging.error(e, exc_info=True)
+            info_container.error('API limit reached. Please try again in 13 seconds.')
     else:
+        # NOTE: When exceeding API limits, the repsonse code is still 200,
+        # but the response body contains an error message. Therefore, we
+        # catch the error here and display it to the user.
         logging.error('Ticker not found. Please try again.')
     
 
 
 
-with container_one:
+# Input Container
+with input_container:
     st.text_input('Enter a ticker', value='AAPL', max_chars=5, key='ticker')
     st.button('Submit', key='submit_button')
 
+# Load data when the submit button is clicked
 if st.session_state.submit_button:
     load_data(st.session_state.ticker)
